df7d163a13bcf9f783b8e285dcce0cea1cb8b1ba
benhurimmanuel <benhurimmanuel@gmail.com>
2023-03-15 18:16:33 +0530

    completed

--- app.js
+++ app.js
@@ -10,12 +10,19 @@ fs.createReadStream("./data.csv", 'utf-8')
         const rows = data.split("\n")
         const titleRow = rows[0]
         const dataList = rows.slice(1, rows.length)
+
         let singleElement = {}
         let allElements = []
+
         for (i in dataList) {
             for (j in dataList[i].split(",")) {
-                singleElement[titleRow.split(",")[j]] = dataList[i].split(",")[j]
-
+                let singleEntry = dataList[i].split(",")[j]
+                if (singleEntry == "") { singleEntry = null }
+                if (JSON.stringify(singleEntry).includes("-")) {
+                    let arrayElements = JSON.stringify(singleEntry).split("-")
+                    singleEntry = arrayElements.toString().replace(/"/g, "").split(",")
+                }
+                singleElement[titleRow.split(",")[j]] = singleEntry
                 const size = Object.keys(singleElement).length;
                 if (size == titleRow.split(",").length) {
                     allElements.push(singleElement)
@@ -23,34 +30,10 @@ fs.createReadStream("./data.csv", 'utf-8')
                 }
             }
         }
-
-
         //writing to file
         fs.writeFileSync("./data.json", JSON.stringify(allElements))
     })
 
 
-// const rows = require("./asd.json")
 
-// const titleRow = rows[0]
-// const dataList = rows.slice(1, rows.length)
-// let singleElement = {}
-// let allElements = []
-// for (i in dataList) {
-//     for (j in dataList[i].split(",")) {
-//         singleElement[titleRow.split(",")[j]] = dataList[i].split(",")[j]
-
-//         const size = Object.keys(singleElement).length;
-//         if (size == titleRow.split(",").length) {
-//             allElements.push(singleElement)
-//             singleElement = {}
-//         }
-//     }
-// }
-
-// console.log(allElements);
-
-
-
-                // Candidates will not be allowed to enter the exam venue after entry closing time. Candidates should carry 2 latest passport size photographs and an original valid photo identity card having the same date of birth as printed on the admit card. If the Photo ID card does not have the Date of Birth then the candidate must carry an additional certificate also in original as proof of their Date of Birth, failing which they will not be admitted for the exam. In case of any mismatch in the date of birth, candidate will not be admitted for the exam.
 
